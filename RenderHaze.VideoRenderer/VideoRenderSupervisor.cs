cd28b7a3b940e6a2941883fdb53a320508625535
Cain Atkinson <yellowsink@protonmail.com>
2021-10-06 20:07:55 +0100

    I am a bit dumb

--- RenderHaze.VideoRenderer/VideoRenderSupervisor.cs
+++ RenderHaze.VideoRenderer/VideoRenderSupervisor.cs
@@ -20,21 +20,32 @@ namespace RenderHaze.VideoRenderer
 			Framerate = framerate;
 		}
 
-		public void RenderVideoTo(string outputPath, string? audioPath, EventHandler<(int, int)>? progress)
+		public void RenderVideoTo(string outputPath, string? audioPath, EventHandler<RenderProgressReport>? progress)
 		{
 			var tmpPath = Path.Combine(TmpPath.DefaultTempLocation, "renderhaze_frames");
+
+			EventHandler<(int, int)>? progressFunc = progress != null
+														 ? (s, p) => progress.Invoke(s,
+															 new RenderProgressReport
+															 {
+																 Total = p.Item1, Completed = p.Item2,
+																 Type  = RenderProgressType.Rendering
+															 })
+														 : null;
 			
-			var frameRenderer = new FrameRenderer<TPixel>();
-			frameRenderer.ObjectTimelines = Timelines.ToList();
-			frameRenderer.RenderFramesToDisk((int) Width, (int) Height, tmpPath, progress);
+			var frameRenderer = new FrameRenderer<TPixel>
+			{
+				ObjectTimelines = Timelines.ToList()
+			};
+			frameRenderer.RenderFramesToDisk((int) Width, (int) Height, tmpPath, progressFunc);
 
 			var files = new DirectoryInfo(tmpPath).EnumerateFiles()
 												  .Select(f => f.FullName)
 												  .OrderBy(fn => fn)
 												  .ToArray();
 			
+			progress?.Invoke(this, new RenderProgressReport{Type = RenderProgressType.Encoding});
 			Encoder.Encode(files, outputPath, Framerate, audioPath).Wait();
-			
 			Directory.Delete(tmpPath, true);
 		}
 	}
