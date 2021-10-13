63fe2a8543083c325da417d79fabb94c9b06f43a
Cain Atkinson <yellowsink@protonmail.com>
2021-10-13 21:22:33 +0100

    apparently it just works now

--- VideoRenderer.Testing/UnitTest1.cs
+++ VideoRenderer.Testing/UnitTest1.cs
@@ -15,25 +15,27 @@ namespace VideoRenderer.Testing
 		[Test]
 		public void Test1()
 		{
+			File.Delete(Path.Combine(Environment.CurrentDirectory, "test_out.mp4"));
+			
 			var test1 = Image.Load<Rgba64>(Path.Combine(Environment.CurrentDirectory, "test_1.png"));
 			var test2 = Image.Load<Rgba64>(Path.Combine(Environment.CurrentDirectory, "test_2.png"));
 
 			var bgTimeline = new Timeline<Rgba64>(new Object<Rgba64>(test2),
-												  100,
+												  5, //100,
 												  new[]
 												  {
 													  new TimePoint(0, 0, 0, 1)
 												  });
 
 			var timeline1 = new Timeline<Rgba64>(new Object<Rgba64>(test1),
-												 100,
+												 5, //100,
 												 new[]
 												 {
 													 new TimePoint(0,  60,   50,  1),
-													 new TimePoint(20, 500,  600, .75f),
+													 /*new TimePoint(20, 500,  600, .75f),
 													 new TimePoint(49, 200,  100, .9f),
 													 new TimePoint(75, 1500, 500, .25f),
-													 new TimePoint(99, 0,    0,   0)
+													 new TimePoint(99, 0,    0,   0)*/
 												 });
 
 			var timelines = new[] { bgTimeline, timeline1 };
